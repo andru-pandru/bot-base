2270f35e1544e8f3b6f6a5adae28b02d53f52a7f
andrii.rychkov@gmail.com <andrii.rychkov@gmail.com>
2023-04-26 15:32:58 +0000

    change lib

--- tbot.py
+++ tbot.py
@@ -1,10 +1,31 @@
 #telebot
-import telegram
-from telegram.ext import Updater, CommandHandler
+import telebot
+import config
 
-bot = telegram.Bot(token='5352998015:AAH_st69lxHb3EMf0AnqLjhLWB_gDFAEsDo')
 
-def start(update, context):
-    context.bot.send_message(chat_id=update.message.chat_id, text="Hello")
+#with open('token.txt', 'r') as file:
+#    TOKEN = file.read()
 
-start_handler = CommandHandler('start', start)
+#print((TOKEN))
+
+bot = telebot.TeleBot(config.TOKEN)
+
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    sti = open(static/welcom.webp, 'rb')
+    bot.send_stiker(message.chat.id, sti)
+
+    bot.send_message(message.chat.id, "hello, {0.first_name}!\n I am a </b>{1.first_name}</b> bot".format(message.from_user, bot.get_me()),parse_mode='html')
+
+
+    
+
+
+@bot.message_handler(content_types=['text'])
+
+def msg(message):
+    bot.send_message(message.chat.id, message.text)
+
+#run
+bot.polling(none_stop=True)
+
