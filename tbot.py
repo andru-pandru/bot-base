74567fe42214b218043dab0bfd65b8117e533f24
andrii.rychkov@gmail.com <andrii.rychkov@gmail.com>
2023-04-26 14:12:57 +0000

    2

--- tbot.py
+++ tbot.py
@@ -1,1 +1,10 @@
 #telebot
+import telegram
+from telegram.ext import Updater, CommandHandler
+
+bot = telegram.Bot(token='5352998015:AAH_st69lxHb3EMf0AnqLjhLWB_gDFAEsDo')
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id, text="Hello")
+
+start_handler = CommandHandler('start', start)
