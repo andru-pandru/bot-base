d3cda577c6d8de6ad863d86d497c931ea1050fdc
andrii.rychkov@gmail.com <andrii.rychkov@gmail.com>
2023-04-26 19:40:15 +0000

    change3

--- tbot.py
+++ tbot.py
@@ -1,30 +1,85 @@
 #telebot
+import pymysql.cursors
 import telebot
 import config
 
-
-#with open('token.txt', 'r') as file:
-#    TOKEN = file.read()
-
-#print((TOKEN))
-
 bot = telebot.TeleBot(config.TOKEN)
+
+
+import pymysql.cursors
+
+def execute_sql_commands(sql_commands):
+    # Establish a connection to the MySQL database
+    connection = pymysql.connect(host='your_host',
+                                 user='your_username',
+                                 password='your_password',
+                                 db='your_database',
+                                 charset='utf8mb4',
+                                 cursorclass=pymysql.cursors.DictCursor)
+
+    try:
+        # Execute the SQL commands
+        with connection.cursor() as cursor:
+            for command in sql_commands:
+                cursor.execute(command)
+
+        # Commit the changes
+        connection.commit()
+
+        # Fetch any remaining results
+        with connection.cursor() as cursor:
+            result = cursor.fetchall()
+
+    except Exception as e:
+        # Roll back the transaction if an error occurs
+        connection.rollback()
+        print('Error executing SQL commands: {}'.format(e))
+        result = None
+
+    finally:
+        # Close the connection
+        connection.close()
+
+    # Return the results (or None if there was an error)
+    return result
+
+
+@bot.message_handler(commands=['last'])
+
+
+
+
+"""
+def get_data(sql_commands):
+    connection = pymysql.connect  (user=config.mysqluser,
+                                password=config.mysqlpassword,
+                                host=config.mysqlhost, 
+                                database=config.mysqldatabase,
+                                charset='utf8mb4',
+                                cursorclass=pymysql.cursors.DictCursor)
+    
+    print('1')
+    
+  #  sql_commands = ("use bot_base","select * from tasks")
+
+    with connection.cursor() as cursor:
+        for command in sql_commands:
+            cursor.execute(command)
+            data = cursor.fetchall()
+
+    connection.close()
+    bot.send_message(message.chat.id, data)
+"""
+
+
+
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
-    sti = open(static/welcom.webp, 'rb')
-    bot.send_stiker(message.chat.id, sti)
+    sti = open('static/yodda.webp', 'rb')
+    bot.send_sticker(message.chat.id, sti)
+    bot.send_message(message.chat.id,"hello")
 
-    bot.send_message(message.chat.id, "hello, {0.first_name}!\n I am a </b>{1.first_name}</b> bot".format(message.from_user, bot.get_me()),parse_mode='html')
-
-
-    
-
-
-@bot.message_handler(content_types=['text'])
-
-def msg(message):
-    bot.send_message(message.chat.id, message.text)
 
 #run
 bot.polling(none_stop=True)
