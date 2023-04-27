82a97f0b12bfc293b3bb354dff0c92556ee252c5
andrii.rychkov@gmail.com <andrii.rychkov@gmail.com>
2023-04-27 14:18:26 +0000

    changed output

--- tbot.py
+++ tbot.py
@@ -34,11 +34,13 @@ def execute_sql_command(sql_command):
 @bot.message_handler(commands=['last'])
 def base_init(message):
     execute_sql_command("use bot_base")
-    result = execute_sql_command("select task_name from tasks ORDER BY id DESC LIMIT 1")
-    bot.send_message(message.chat.id,result)
-    bot.send_message(message.chat.id,"----------------------")
-    result = execute_sql_command("select task from tasks ORDER BY id DESC LIMIT 1")
-    bot.send_message(message.chat.id,result)
+    res = execute_sql_command("select task from tasks ORDER BY id DESC LIMIT 1")
+    result='<b>'+(str(res))+'</b>'
+    print(result)
+    bot.send_message(message.chat.id,result,parse_mode='HTML')
+    #pic = execute_sql_command("select link from tasks order by id desc limit 1")
+    picture = open('static/describe.jpg','rb')
+    bot.send_photo(message.chat.id,picture)
 
 
 
